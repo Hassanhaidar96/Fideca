b8355f64980776efcec6aad30f684e51d2a60c12
Hassanhaidar96 <104452064+Hassanhaidar96@users.noreply.github.com>
2025-02-24 15:21:22 +0100

    Add files via upload

--- Fideca.py
+++ Fideca.py
@@ -155,13 +155,13 @@ if st.sidebar.button("Run Analysis"):
         Ksys_Fideca1_2 = 2.6
         Ksys_Fideca1_3 = 2.6
         
-    
+        
         if  dv_0/8 < Cu:
             Ksys_Fideca1_1 = 4.4 - (14.4/dv_0)*Cu
-        if  dv_0 < 220:
-            Ksys_Fideca1_2 = 0.01 * dv_0 +0.4
-    
-    
+        # if  dv_0 < 220:
+        #     Ksys_Fideca1_2 = 0.01 * dv_0 +0.4
+            
+        
         Ksys_Fideca1 = min(Ksys_Fideca1_1,Ksys_Fideca1_2,Ksys_Fideca1_3)
     
         
